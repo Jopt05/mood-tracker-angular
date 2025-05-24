a3f09d408eab8ac05fc8197053ac1d150815fb09
Jesus Puentes <jopt98@gmail.com>
2025-05-24 08:23:02 -0600

    animaciones

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiKey: 'https://mood-tracker-app-backend-ntsz.onrender.com/api',
   quotesApiKey: 'ighpEtaqcmX/0uNEq0wl0Q==7BbpDXIqkxXUvV0H',
-  getQuotes: false
+  getQuotes: true
 };
